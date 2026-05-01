e781595994dfd310da78193284d1f3520885f490
vladb84 <125182705+vladb84@users.noreply.github.com>
2026-05-01 16:03:30 +0300

    feat: add firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
 const firebaseConfig = {
   apiKey: "AIzaSyBgOJpe3nIsEyH2zQzcxNd--iYS3ubqdwA",
   authDomain: "school-diary-c4808.firebaseapp.com",
@@ -10,7 +9,6 @@ const firebaseConfig = {
   messagingSenderId: "692563221139",
   appId: "1:692563221139:web:760790bb74fb1ae5e40b66"
 };
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
